83ed1f1a3f2234864b773906327aa2d0f24ab256
Robin Schaaf <robinmschaaf@gmail.com>
2012-11-20 13:45:07 -0500

    events controller

--- www/js/index.js
+++ www/js/index.js
@@ -44,7 +44,6 @@ var app = {
     	}
 
 
-	alert('deviceready !  yay!');
 	checkConnection();
 
 	
@@ -77,7 +76,7 @@ $(document).bind('pageshow', function(event){
  
  
  $('#navHeader').remove();
- $('[data-role="page"]').prepend("<div class='ui-bar ui-bar-b' id='navHeader'>I'm just a div with bar classes</div>");
+ //$('[data-role="page"]').prepend("<div class='ui-bar ui-bar-b' id='navHeader'>I'm just a div with bar classes</div>");
  $('body').prepend("<div class='ui-bar ui-bar-b' id='navHeader'>I'm just a div with bar classes</div>");
     
 });
@@ -85,11 +84,11 @@ $(document).bind('pageshow', function(event){
 
 $('#eventsPage').live('pagecreate',function(event, ui){
 	
-	$('#eventsData').html('HERE');
+	$('#eventsData').html('PAGECREATE');
 	
 
     $.get('http://mpprd.library.nd.edu/events', function(data) {
-	$('#eventsData').html(data);
+	$('#eventsData').html('DATARECV');
 	alert("get" + data);
     });                            
 
@@ -131,9 +130,3 @@ function checkConnection() {
 
 }
 
-
-function updateEvents(){
-alert("events");
-	$('#eventsData').html("AAAA");
-
-}
