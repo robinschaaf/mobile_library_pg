561d43f425bfd09298491e8833300f78392443ec
Robin Schaaf <robinmschaaf@gmail.com>
2012-12-07 10:01:58 -0500

    working on onesearch

--- www/js/index.js
+++ www/js/index.js
@@ -328,8 +328,8 @@ function updateIFrame(){
 	var js = "<script>function openChildBrowser(url){alert ('cb');}</script>";
 	
 	//append javascript for opening childbrowser since javascript here isn't available there
-	$('#iframeSource').contents().find('body').append(js);
-	alert ($('#iframeSource').contents().find('body').html());
+	//$('#iframeSource').contents().find('body').append(js);
+	//alert ($('#iframeSource').contents().find('body').html());
 
 
 	$('#iframeSource').css("height","100%");
@@ -348,7 +348,7 @@ function updateIFrame(){
 		var u = $.mobile.path.parseUrl( val );
 	
 		if (isExtLink(u)){
-			return "javascript:alert('" + val + "');";
+			return "javascript:window.parent.plugins.childBrowser.showWebPage('" + val + "');";
 		}else{
 			return val;
 		}
