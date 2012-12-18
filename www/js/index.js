977a361097bbd650431be8aeb0d43e817575e0d4
Robin Schaaf <robinmschaaf@gmail.com>
2012-12-18 16:55:36 -0500

    iframe width

--- www/js/index.js
+++ www/js/index.js
@@ -294,7 +294,7 @@ function showIFrame( sourceURL, origURLObj, options ) {
 				//load into an iframe
 				//and expand the width of the content container (parents)
 
-				$page.find('.subPageData').append( "<iframe id='iframeSource' onload='updateIFrame();' frameborder='0' src = '" + sourceURL + "'></iframe>" ).parents().css('padding', '0px', 'margin', '0px');
+				$page.find('.subPageData').append( "<iframe id='iframeSource' onload='updateIFrame();' style='height:0px;width:250px;' frameborder='0' src = '" + sourceURL + "'></iframe>" ).parents().css('padding', '0px', 'margin', '0px');
 
 				$page.page();
 
@@ -369,7 +369,8 @@ alert('after hide');
 }
 
 
-$("#iframeSource").ready(function (){
+
+$('#iframeSource').live('ready', function () {
 	alert('iframe ready');
 });
 
