e7cb13a999322bec775cd32506f74a05a3f0a8de
Robin Schaaf <robinmschaaf@gmail.com>
2012-12-19 14:06:44 -0500

    popup fix

--- www/js/index.js
+++ www/js/index.js
@@ -86,7 +86,7 @@ $(document).bind('pageinit', function(e, data){
         popupbeforeposition: function() {
             var maxHeight = $( window ).height() - 30 + "px";
             $("#popupMap img").css( "max-height", maxHeight );
-            $('#popupMap').popup("open")
+            alert($('#popupMap').attr('class'))
             $.mobile.loading( 'hide' );
         }     
         
